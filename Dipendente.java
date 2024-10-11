a5afd8ff67e225ba5e896c158be075b474bdc69e
Carleoinserra <inserracarlo@gmail.com>
2024-10-11 14:41:12 +0200

    Add files via upload

--- Dipendente.java
+++ Dipendente.java
@@ -1,15 +1,22 @@
 
 public class Dipendente {
 	
+	
+	// definiamo 3 proprietà due di tipo stringa e uan di tipo int
 	String cognome;
 	String mansione;
 	int stipendio;
+	
+	
+	// nel costruttore di dipendente andiamo ad assegna i valori ottenuti da parametro alle propreità
 	public Dipendente(String cognome, String mansione, int stipendio) {
 		
 		this.cognome = cognome;
 		this.mansione = mansione;
 		this.stipendio = stipendio;
 	}
+	
+	// rappresentazione testuale di un oggetto dipendente
 	@Override
 	public String toString() {
 		return "Dipendente [cognome=" + cognome + ", mansione=" + mansione + ", stipendio=" + stipendio + "]";
